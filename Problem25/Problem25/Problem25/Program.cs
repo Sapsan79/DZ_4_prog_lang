33e031967c4a745297e1ab6ea177d6d0e0899105
Sapsan <semya_s@mail.ru>
2022-12-18 22:53:47 +0300

    Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.

---
+++ Problem25/Problem25/Problem25/Program.cs
@@ -0,0 +1,24 @@
+﻿//Задача 25: Напишите цикл, который принимает
+//на вход два числа (A и B) и возводит число A
+//в натуральную степень B.
+
+int A = ReadInt();
+int B = ReadInt();
+int result = 1;
+for (int j = 0; j < B; j++)
+{
+    result *= A;
+}
+    Console.WriteLine(result);
+
+int ReadInt()
+{
+    Console.WriteLine("Введите число: ");
+    if (int.TryParse(Console.ReadLine(), out int i))
+    {
+        return i;
+    }
+    Console.WriteLine("Вы ввели не число");
+    return -1;
+
+}
